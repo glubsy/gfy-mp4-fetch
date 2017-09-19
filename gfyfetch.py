8d00e3f8e7f61ae2c4e4d19e971cc402cd32a3d9
glubsy <glubsy@users.noreply.github.com>
2017-09-19 20:39:08 +0200

    Ditching urrlib.urlretrieve in favour of requests in order to spoof UA (seems to work)

--- gfyfetch.py
+++ gfyfetch.py
@@ -4,17 +4,20 @@ import subprocess
 #import fnmatch
 #import shutil
 import urllib
-#import requests
+import requests
 import re
 from tqdm import tqdm
 #import json
 #from urllib import urlopen
 from gfycat.client import GfycatClient
 from gfycat.error import GfycatClientError
+# urllib.request.URLopener.version = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
 QUERY_ENDPOINT = 'http://gfycat.com/cajax/get/'
-TMP_FILELIST= '/tmp/gfyfetch_filelist.txt'
+TMP_FILELIST = '/tmp/gfyfetch_filelist.txt'
 GLOBAL_URL_OBJECT = []
 CWD = os.getcwd()
+request_session = requests.Session()
+request_session.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/54.0'})
 
 def print_usage():
     """Prints script usage"""
@@ -56,6 +59,7 @@ def scan_directory(inDIR):
     fileList = []
     fileIdSet = set()
     count = 0
+    dupecount = 0
     current_file_props = []
     # Walk through directory
     for dName, sdName, fList in os.walk(inDIR):
@@ -71,12 +75,14 @@ def scan_directory(inDIR):
                     fileIdSet.add(parse_path_line(fileName)[2]) # add file id to set
                     fileList.append(os.path.join(dName, fileName)) # add file path to list
                     count += 1
+                else:
+                    dupecount += 1
             # else:
             #     print("filename", fileName, "doesn't match.")
     fileList.sort()
-    print("fileList:", fileList)
-    print("fileIdSet:", fileIdSet)
-    print("count:", count)
+    # print("fileList:", fileList)
+    # print("fileIdSet:", fileIdSet)
+    print("dupecount:", dupecount)
     write_list_to_file(fileList)
 
 
@@ -116,16 +122,17 @@ def read_file_listing(file):
 
 
 def loop_through_text_file(file):
-    """main iterating loop"""
+    """Main iterating loop"""
 
-    dir_id_pair = read_first_line(file)
-    setup_download_dir(dir_id_pair[0])
+    dir_id_pair = read_first_line(file) #retrieve parent_dir/file_id from text list
+    setup_download_dir(dir_id_pair[0]) #create our download directory if doesn't exist FIXME add option to set manually instead of CWD
 
-    GLOBAL_URL_OBJECT.append(dir_id_pair[0]) #download_dir #FIXME instead of append, create the list with empty elements and change them by index
-    GLOBAL_URL_OBJECT.append(dir_id_pair[1]) #file_id
+    GLOBAL_URL_OBJECT.extend(dir_id_pair) #FIXME instead of append/extend, create the list with empty elements and change them by index
+    
     if process_id(GLOBAL_URL_OBJECT[0], GLOBAL_URL_OBJECT[1]):
-        remove_first_line(file) # Only remove after download succeeds TODO
-
+        remove_first_line(file) #TODO only remove after download succeeds
+    else:
+        print(BColors.FAIL + "Download of " + GLOBAL_URL_OBJECT[1] + "failed! Reason: " + GLOBAL_URL_OBJECT[2] + BColors.ENDC)
 
 def read_first_line(file):
     """Only read the first line of file"""
@@ -166,42 +173,55 @@ def parse_path_line(theline):
 def process_id(download_dir, file_id):
     """Process the current filename"""
 
-    print("process_id() download_dir:", download_dir, "file_id:", file_id)
     #GLOBAL_URL_OBJECT: [ "download_dir", "file_id", "fetched_url_mp4" ]
-    GLOBAL_URL_OBJECT.append(gfycat_client_fetcher(file_id))
+    if not gfycat_client_fetcher(file_id):
+        return False
 
     generate_dest_filename(GLOBAL_URL_OBJECT[0], GLOBAL_URL_OBJECT[1])
 
     if GLOBAL_URL_OBJECT[2] != None:
-        file_downloader(GLOBAL_URL_OBJECT[2])
+        if file_downloader(GLOBAL_URL_OBJECT[2]):
+            return True
+        else:
+            return False
     else:
-        pass
+        return False
 
 def gfycat_client_fetcher(arg):
     """Uses the gfycat.client library to fetch JSON, returns mp4Url"""
-    client = GfycatClient()
+    client = GfycatClient() #TODO: copy over gfycat.client code and spoof UA in requests
 
     try:
         myquery = client.query_gfy(arg) #dict
     except GfycatClientError as error:
         print(error.error_message)
         print(error.status_code)
+        return False
 
     try:
         if 'error' in myquery: # (myquery['error']):
-            print(BColors.FAIL + "WARNING:", myquery['error'], BColors.ENDC)
+            print(BColors.FAIL + "JSON Fetcher Warning:", myquery['error'], BColors.ENDC)
+            GLOBAL_URL_OBJECT.append(myquery['error'])
+            return False
         else:
             print(BColors.OKGREEN + "mp4Url value = ", myquery['gfyItem']['mp4Url'], BColors.ENDC)
-            return myquery['gfyItem']['mp4Url']
+            GLOBAL_URL_OBJECT.append(myquery['gfyItem']['mp4Url'])
+            return True
     except:
-        print(BColors.FAIL + "There was an error: ", ValueError)
+        print(BColors.FAIL + "JSON Fetcher exception error: " + ValueError + BColors.ENDC)
+        return False
+    return False
 
-# def new_json_parser():
+# def gfycat_fetcher():
 #     """Standalone fetching of JSON, returns mp4Url"""
 #     j = urllib.request.urlopen(QUERY_ENDPOINT+TESTFILEID)
 #     j_obj = json.load(j)
 #     print("mp4Url value = ", j_obj['gfyItem']['mp4Url'])
 #     return j_obj['gfyItem']['mp4Url']
+
+
+
+
 
 class TqdmUpTo(tqdm):
     """Provides `update_to(n)` which uses `tqdm.update(delta_n)`."""
@@ -218,18 +238,59 @@ class TqdmUpTo(tqdm):
             self.total = totalsize
         self.update(block * blocksize - self.n)  # will also set self.n = b * bsize
 
-        if int(block * blocksize * 100 / totalsize) == 100:
-            print(BColors.BOLD + "Download completed!" + BColors.ENDC) #FIXME: what if no totalsize?
+        # if int(block * blocksize * 100 / totalsize) == 100:
+        #     tqdm.write(BColors.BOLD + "Download completed!" + BColors.ENDC)
+
+
+def file_downloader2(url):
+    """Downloads the file at the url passed."""
+
+    # user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+    # myheaders = {'User-Agent': user_agent}
+    dst = generate_dest_filename(GLOBAL_URL_OBJECT[0], GLOBAL_URL_OBJECT[1])
+    
+    with TqdmUpTo(unit='B', unit_scale=True, miniters=1, desc=url.split('/')[-1]) as t:  # all optional kwargs
+        local_filename, headers = urllib.request.urlretrieve(url, filename=dst, reporthook=t.update_to, data=None)
+    
+    print("contentlength: ", headers['Content-Length'])
+    checkfile = open(local_filename)
+    if checkfile:
+        tqdm.write(BColors.BOLD + "Download completed!" + BColors.ENDC)
+    checkfile.close()
+
+    if os.path.exists(local_filename):
+        return True
+    else:
+        return False
+    return False
 
 
 
 def file_downloader(url):
     """Downloads the file at the url passed."""
-
     dst = generate_dest_filename(GLOBAL_URL_OBJECT[0], GLOBAL_URL_OBJECT[1])
-    print("dest: ", dst)
-    with TqdmUpTo(unit='B', unit_scale=True, miniters=1, desc=url.split('/')[-1]) as t:  # all optional kwargs
-        urllib.request.urlretrieve(url, filename=dst, reporthook=t.update_to, data=None)
+    print("dst: ", dst)
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/536.36'}
+
+    # url="http://httpbin.org/headers" #for testing
+    chunkSize = 1024
+
+    r = request_session.get(url, headers=headers, stream=True)
+    if r.status_code != 200:
+        tqdm.write(BColors.FAIL + "Error fetching the URL: " + r.status_code + BColors.ENDC)
+        return
+    with open(dst, 'wb') as f:
+        pbar = tqdm(unit="B", total=int(r.headers['Content-Length']))
+        for chunk in r.iter_content(chunk_size=chunkSize):
+            if chunk: # filter out keep-alive new chunks
+                pbar.update(len(chunk))
+                f.write(chunk)
+    tqdm.write(BColors.BOLD + "\nDownload completed!" + BColors.ENDC)
+    return dst
+
+
+
+
 
 
 
@@ -247,22 +308,22 @@ def generate_dest_filename(download_dir, file_id):
     return download_dest
 
 
-def setup_download_dir(dir):
+def setup_download_dir(direrctory):
     """Setup local directory structure for downloads"""
-    if not os.path.exists(CWD + os.sep + dir):
-        os.makedirs(CWD + os.sep + dir)
+    if not os.path.exists(CWD + os.sep + direrctory):
+        os.makedirs(CWD + os.sep + direrctory)
 
 
-def setup_use_dir(dir):
+def setup_use_dir(direrctory):
     """Starts logic to scan supplied dir path"""
-    print("Scanning directory:", dir)
-    scan_directory(dir)
+    print("Scanning directory:", direrctory)
+    scan_directory(direrctory)
     if read_file_listing(TMP_FILELIST):
         loop_through_text_file(TMP_FILELIST)
 
 def setup_use_file(file):
     """Starts logic to use a supplied file list"""
-    print("Using file list", file, "as file listing.")
+    print("Using supplied file list '", file, "' as file listing.")
     if read_file_listing(file):
         loop_through_text_file(file)
 
@@ -271,8 +332,10 @@ def setup_prompt_resume():
     question = BColors.HEADER + "Warning: a previous file listing is present in " + TMP_FILELIST + "\nWould you like to load listing from it?" + BColors.ENDC
     if query_yes_no(question):
         setup_use_file(TMP_FILELIST)
+        return True
     else:
-        pass
+        return False
+
 
 def previous_tmp_file_exists():
     """Checks if a previously generated file listing is in default location"""
@@ -316,7 +379,8 @@ def main():
         arg1 = str(sys.argv[1])
 
     if previous_tmp_file_exists():
-        setup_prompt_resume()
+        if setup_prompt_resume():
+            return
 
     if is_first_arg_dir(arg1):
         setup_use_dir(arg1)
